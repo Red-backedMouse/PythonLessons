46e82f9618c17d9c0364baa877279a3a5a7b6e74
Ekaterina <ekormashova.gmail.com>
2024-04-25 01:37:47 +0300

    Edit cycle "while" on cycle "for"

--- lesson_1/nambersCycle.py
+++ lesson_1/nambersCycle.py
@@ -1,6 +1,5 @@
 x = int(input("Ведите целое, положительное число: "))
 count = 1
-while count <= x:
-    if count % 2 == 0:
-        print(f"{count = }")
-    count += 1
+for i in range(1, x + 1):
+    if i % 2 == 0:
+        print(f"{i=}")
